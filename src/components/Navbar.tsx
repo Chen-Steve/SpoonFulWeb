79ccba8ea9dcdd11f239c2c0977de092bfe570b3
Steven <zhangc2@illinois.edu>
2024-02-07 19:16:52 -0600

    updated spoonful logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
+//import { Sprout } from 'lucide-react';
+
 import Link from 'next/link';
 import { buttonVariants } from './ui/button';
-import { HandMetal } from 'lucide-react';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import UserAccountNav from './UserAccountNav';
@@ -8,12 +9,14 @@ import UserAccountNav from './UserAccountNav';
 const Navbar = async () => {
   const session = await getServerSession(authOptions);
   return (
-    //<div className=' bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
-    <div className='bg-navbar-bg py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
-      <div className='container flex items-center justify-between'>
-        <Link href='/'>
-          <HandMetal />
-        </Link>
+    <div className='bg-white py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0' style={{ height: '80px' }}>
+      <div className='container flex items-center justify-between' style={{ height: '100%' }}>
+        <div className="relative" style={{ zIndex: 1, height: '100%' }}>
+          <Link href='/'>
+            {/* Adjust the image container for better positioning and scalability */}
+            <img src='/SpoonFulNew256-9.png' alt='SpoonFul Icon' style={{ margin: '-63px', maxHeight: '350%', maxWidth: '200%', height: 'auto' }}/> 
+          </Link>
+        </div>
         <div className='flex-grow space-x-4 flex justify-end mr-4'>
           <Link className={buttonVariants()} href='/'>
             About
@@ -29,7 +32,6 @@ const Navbar = async () => {
             Sign in
           </Link>
         )}
-
       </div>
     </div>
   );
