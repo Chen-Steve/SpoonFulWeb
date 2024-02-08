0bedcd6c9cbf58549b7c8cb75eb2c5af12a21c36
Steven <zhangc2@illinois.edu>
2024-02-07 19:25:08 -0600

    test - precise control of navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,49 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import UserAccountNav from './UserAccountNav';
 
+const styles = {
+  navbar: {
+    height: '80px',
+    backgroundColor: 'white',
+    borderBottom: '1px solid #ececec',
+    position: 'fixed',
+    width: '100%',
+    zIndex: '10',
+    top: '0',
+  },
+  logoContainer: {
+    zIndex: 1,
+    height: '80%',
+  },
+  logo: {
+    margin: '0 auto', // Center logo, adjust as necessary
+    maxHeight: '40%', // Start with a reasonable size, adjust for mobile
+    maxWidth: '40%',
+    height: 'auto',
+  },
+  container: {
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  menu: {
+    display: 'flex',
+    justifyContent: 'end',
+    marginRight: '4rem',
+    gap: '1rem',
+  },
+};
+
 const Navbar = async () => {
   const session = await getServerSession(authOptions);
   return (
     <div className='bg-white py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0' style={{ height: '80px' }}>
-      <div className='container flex items-center justify-between' style={{ height: '100%' }}>
-        <div className="relative" style={{ zIndex: 1, height: '100%' }}>
+      <div className='container' style={styles.container}>
+        <div className="relative flex items-center" style={styles.logoContainer}>
           <Link href='/'>
-            {/* Adjust the image container for better positioning and scalability */}
-            <img src='/SpoonFulNew256-9.png' alt='SpoonFul Icon' style={{ margin: '-63px', maxHeight: '350%', maxWidth: '200%', height: 'auto' }}/> 
+            {/* Adjust logo size for mobile */}
+            <img src='/SpoonFulNew256-9.png' alt='SpoonFul Icon' style={styles.logo}/> 
           </Link>
         </div>
         <div className='flex-grow space-x-4 flex justify-end mr-4'>
